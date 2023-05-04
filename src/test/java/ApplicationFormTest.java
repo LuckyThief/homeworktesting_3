5dae64e363d217601cc592526d6538c6995ed46c
LuckyThief <luckythief13@gmail.com>
2023-05-04 14:30:42 +0400

    Fixed

--- src/test/java/ApplicationFormTest.java
+++ src/test/java/ApplicationFormTest.java
@@ -1,4 +1,4 @@
-import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.exactText;
@@ -6,6 +6,11 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 class ApplicationFormTest {
+
+    @BeforeEach
+    void setUpTests() {
+        open("http://localhost:9999/");
+    }
 
     @Test
     void testFormV1() {
